43095a615b56ce74cec01b75a1b7f24153f63ab1
Pavel Pavlov <pavelsvpavlov@gmail.com>
2023-01-21 11:13:51 +0200

    add welcome message and message for wrong user input

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,17 +1,26 @@
 import random
+drawing = """
+╔═╗┬ ┬┌─┐┌─┐┌─┐  ┌─┐  ┌┐┌┬ ┬┌┬┐┌┐ ┌─┐┬─┐
+║ ╦│ │├┤ └─┐└─┐  ├─┤  ││││ ││││├┴┐├┤ ├┬┘
+╚═╝└─┘└─┘└─┘└─┘  ┴ ┴  ┘└┘└─┘┴ ┴└─┘└─┘┴└─
+"""
+print("Welcome to the game", end="")
+print(drawing)
 
 computer_choice = random.randint(1, 100)
+user_input = int(input("Enter a number between 1 and 100: "))
 
-while True:
-    user_input = int(input("Enter a number between 1 and 100: "))
+while user_input != computer_choice:
 
-    if user_input == computer_choice:
-        break
+    if user_input > 100 or user_input < 1:
+        print("Wrong input!")
+    else:
+        if user_input < computer_choice:
+            print("Too low.")
+        elif user_input > computer_choice:
+            print("Too high.")
 
-    if user_input < computer_choice:
-        print("Too low")
-    elif user_input > computer_choice:
-        print("Too high")
+    user_input = int(input("Enter new number: "))
 
 print()
 print("Congratulations!\nYou guessed the number!")
